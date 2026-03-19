ed12f07a6258862b454b812d4c09016d0c5d685f
kawan <kawan.cavalcante@outook.com>
2026-03-19 19:41:31 -0300

    app_cond02

--- appview05/App.js
+++ appview05/App.js
@@ -9,17 +9,17 @@ import { View, Text } from "react-native-web";
 
 function App(){
   return (
-    <View style={{ backgroundColor: 'rgb(33, 13, 121)', height: '100%', width: 300 }}>
+    <View style={{ backgroundColor: 'rgb(97, 73, 207)', height: '100%', width: 300 }}>
 
       {/* View Laranja */}
-      <View style={{ backgroundColor: 'rgb(138, 15, 117)', height: '20%', width: '100%' }}>
+      <View style={{ backgroundColor: 'rgb(150, 56, 134)', height: '20%', width: '100%' }}>
         <Text>Props de Estilo</Text>
         <Text>Autor: Kawan Cavalcante</Text>
         <Text>Data: 26/02/2026</Text>
       </View>
 
       {/* View Amarela */}
-      <View style={{ backgroundColor: 'rgb(24, 170, 43)', height: '70%', width: 100 }}>
+      <View style={{ backgroundColor: 'rgb(24, 153, 170)', height: '70%', width: 100 }}>
         <Text style={{ color: 'red' }}>Props de Estilo</Text>
         <Text>Autor: Kawan Cavalcante</Text>
         <Text>Data: 26/02/2026</Text>
